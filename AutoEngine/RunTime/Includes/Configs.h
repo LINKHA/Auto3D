4ba39a6e466928641000fcd6a6df7e619c37a11b
LINKHA <421698843@qq.com>
2018-08-20 15:52:30 +0800

    #Remove historical legacy screen usage

--- AutoEngine/RunTime/Includes/Configs.h
+++ AutoEngine/RunTime/Includes/Configs.h
@@ -1,17 +1,6 @@
 #pragma once
 #include <string>
 
-/**
-* @ WINDOW_DOT_USE
-* If enable, DOT window will show
-* many debug can draw in DOT window
-* 0 ---- disable
-* 1 ---- enable
-*/
-#ifndef WINDOW_DOT_USE
-	#define WINDOW_DOT_USE 1
-#endif
-
 ; extern const std::string source_path;
 extern const std::string build_path;
 extern const std::string shader_path;
