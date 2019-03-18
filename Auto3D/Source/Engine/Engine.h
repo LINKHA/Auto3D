7e2262a7f92609e2a8862bf707e4ea101f9e720a
LINKHA <421698843@qq.com>
2019-03-18 10:55:42 +0800

    #Add subsystem descriptions

--- Auto3D/Source/Engine/Engine.h
+++ Auto3D/Source/Engine/Engine.h
@@ -16,10 +16,6 @@ public:
 	void Init();
 	/// Engine normal exit
 	void Exit();
-	/// Shut down engine
-	void ShutDownEngine() { _exiting = true; }
-	/// Engine exit flag
-	bool IsExiting()const { return _exiting; }
 	/// Render geometry
 	void Render();
 	/// Sub system update data,  If pause when _minimized -mode return false
@@ -27,6 +23,10 @@ public:
 	/// Frame finish
 	void FrameFinish();
 
+	/// Shut down engine
+	void ShutDownEngine() { _exiting = true; }
+	/// Engine exit flag
+	bool IsExiting()const { return _exiting; }
 	/// Set minimum frames per second. If FPS goes lower than this, time will appear to slow down.
 	void SetMinFps(int fps);
 	/// Set maximum frames per second. The engine will sleep if FPS is higher than this.
@@ -39,7 +39,6 @@ public:
 	void SetPauseMinimized(bool enable);
 	/// Override timestep of the next frame. Should be called in between RunFrame() calls.
 	void SetNextTimeStep(float seconds);
-
 	/// Return whether to pause update events and audio when _minimized.
 	bool GetPauseMinimized() const { return _pauseMinimized; }
 	/// Get timestep of the next frame. Updated by ApplyFrameLimit().
@@ -55,17 +54,29 @@ public:
 	/// Get the timestep for the next frame and sleep for frame limiting if necessary.
 	void ApplyFrameLimit();
 private:
+	/// Manage the subsystem of all resource loads
 	UniquePtr<ResourceCache> _cache;
+	/// ADAPTS the low-level rendering interface as well as the form's rendering function
 	UniquePtr<Graphics> _graphics;
+	/// 3D rendering of the scene
 	UniquePtr<Renderer> _renderer;
+	/// User input management events
 	UniquePtr<Input> _input;
+	/// Engine Log
 	UniquePtr<Log> _log;
+	/// Hierarchical performance profiler subsystem.
 	UniquePtr<Profiler> _profiler;
+	/// Process all engine time, calculate FPS, etc
 	UniquePtr<Time> _time;
+	/// The message management mechanism for the underlying interaction between the game project and the engine
 	UniquePtr<RegisteredBox> _registeredBox;
+	/// Use of game scripts
 	UniquePtr<Script> _script;
+	/// Ui-related operations and rendering capabilities
 	UniquePtr<UI> _ui;
+	/// Physical world and functional storage
 	UniquePtr<Physics> _physics;
+	/// An adapter system that operates on files based on the platform
 	UniquePtr<FileSystem> _fileSystem;
 	//This subsystem is implemented in the Audio component, the first one created
 	//UniquePtr<Audio> _audio;
