d6daf6d0698275666b3f583f62e33a08beae014e
LINKHA <421698843@qq.com>
2019-07-20 17:34:18 +0800

    #Finish start interface

--- AutoEditor/AutoEditor.cpp
+++ AutoEditor/AutoEditor.cpp
@@ -4,6 +4,11 @@
 #include <ThirdParty/Imgui/imgui.h>
 #include <ThirdParty/Imgui/imgui_user/imgui_user.h>
 
+void DrawStartPage()
+{
+
+}
+
 void AutoEditor::Init()
 {
 	auto* graphics = Object::Subsystem<Graphics>();
@@ -20,7 +25,7 @@ void AutoEditor::Update()
 	Vector2I windowSize = window->GetSize();
 
 	{
-		ImGui::PushFont("standard_big");
+		
 		bool state;
 		ImGuiWindowFlags windowFlag = 0;
 		windowFlag |= ImGuiWindowFlags_NoTitleBar;
@@ -28,18 +33,48 @@ void AutoEditor::Update()
 		windowFlag |= ImGuiWindowFlags_NoMove;
 		//windowFlag |= ImGuiWindowFlags_NoBackground;
 		ImGui::Begin("SelectProjectWindow", &state, windowFlag);// Create a window called "Hello, world!" and append into it.
+
 		ImGui::SetWindowPos(ImVec2(0, 0));
 		ImGui::SetWindowSize(ImVec2(windowSize._x, windowSize._y));
 
-		if (ImGui::Button("Project Create",ImVec2(372.0f,200.0f)))
+		ImGui::PushFont("standard_big");
+
+		ImGuiWindowFlags flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove |
+			ImGuiWindowFlags_NoResize | ImGuiWindowFlags_HorizontalScrollbar |
+			ImGuiWindowFlags_NoSavedSettings;
+
+		ImGui::AlignTextToFramePadding();
+		ImGui::TextUnformatted("RECENT PROJECTS");
+		ImGui::Separator();
+		ImGui::BeginGroup();
 		{
+			if (ImGui::BeginChild("projects_content",
+				ImVec2(ImGui::GetContentRegionAvail().x * 0.7f, ImGui::GetContentRegionAvail().y),
+				false, flags))
+			{
+
+				
+			}
+			ImGui::EndChild();
 		}
-		//ImGui::SameLine();
-			
-		if (ImGui::Button("Project Select"))
+		ImGui::EndGroup();
+
+		ImGui::SameLine();
+
+		ImGui::BeginGroup();
 		{
+			if (ImGui::Button("NEW PROJECT", ImVec2(ImGui::GetContentRegionAvailWidth(), 0.0f)))
+			{
+				
+			}
+
+			if (ImGui::Button("OPEN OTHER", ImVec2(ImGui::GetContentRegionAvailWidth(), 0.0f)))
+			{
+				
+			}
 		}
-		//ImGui::SameLine();
+		ImGui::EndGroup();
+
 		ImGui::PopFont();
 
 		ImGui::End();
