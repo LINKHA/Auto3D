6f54b6ebd2e6a7455790c2ba7449709f4dbd81e3
LINKHA <421698843@qq.com>
2019-07-27 11:22:10 +0800

    #Tentatively separate the start interface

--- AutoEditor/AutoEditor.cpp
+++ AutoEditor/AutoEditor.cpp
@@ -4,7 +4,61 @@
 #include <ThirdParty/Imgui/imgui.h>
 #include <ThirdParty/Imgui/imgui_user/imgui_user.h>
 
+void DrawStartPage(const Vector2I& windowSize)
+{
+	bool state;
+	ImGuiWindowFlags windowFlag = 0;
+	windowFlag |= ImGuiWindowFlags_NoTitleBar;
+	windowFlag |= ImGuiWindowFlags_NoResize;
+	windowFlag |= ImGuiWindowFlags_NoMove;
+	//windowFlag |= ImGuiWindowFlags_NoBackground;
+	ImGui::Begin("SelectProjectWindow", &state, windowFlag);// Create a window called "Hello, world!" and append into it.
 
+	ImGui::SetWindowPos(ImVec2(0, 0));
+	ImGui::SetWindowSize(ImVec2(windowSize._x, windowSize._y));
+
+	ImGui::PushFont(UIFont::standard_big);
+
+	ImGuiWindowFlags flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove |
+		ImGuiWindowFlags_NoResize | ImGuiWindowFlags_HorizontalScrollbar |
+		ImGuiWindowFlags_NoSavedSettings;
+
+	ImGui::AlignTextToFramePadding();
+	ImGui::TextUnformatted("RECENT PROJECTS");
+	ImGui::Separator();
+	ImGui::BeginGroup();
+	{
+		if (ImGui::BeginChild("projects_content",
+			ImVec2(ImGui::GetContentRegionAvail().x * 0.7f, ImGui::GetContentRegionAvail().y),
+			false, flags))
+		{
+
+
+		}
+		ImGui::EndChild();
+	}
+	ImGui::EndGroup();
+
+	ImGui::SameLine();
+
+	ImGui::BeginGroup();
+	{
+		if (ImGui::Button("NEW PROJECT", ImVec2(ImGui::GetContentRegionAvailWidth(), 0.0f)))
+		{
+
+		}
+
+		if (ImGui::Button("OPEN OTHER", ImVec2(ImGui::GetContentRegionAvailWidth(), 0.0f)))
+		{
+
+		}
+	}
+	ImGui::EndGroup();
+
+	ImGui::PopFont();
+
+	ImGui::End();
+}
 void AutoEditor::Init()
 {
 	auto* graphics = Object::Subsystem<Graphics>();
@@ -20,61 +74,7 @@ void AutoEditor::Update()
 	auto* window = Subsystem<Graphics>()->RenderWindow();
 	Vector2I windowSize = window->GetSize();
 
-	{
-		
-		bool state;
-		ImGuiWindowFlags windowFlag = 0;
-		windowFlag |= ImGuiWindowFlags_NoTitleBar;
-		windowFlag |= ImGuiWindowFlags_NoResize;
-		windowFlag |= ImGuiWindowFlags_NoMove;
-		//windowFlag |= ImGuiWindowFlags_NoBackground;
-		ImGui::Begin("SelectProjectWindow", &state, windowFlag);// Create a window called "Hello, world!" and append into it.
-
-		ImGui::SetWindowPos(ImVec2(0, 0));
-		ImGui::SetWindowSize(ImVec2(windowSize._x, windowSize._y));
-
-		ImGui::PushFont(UIFont::standard_big);
-
-		ImGuiWindowFlags flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove |
-			ImGuiWindowFlags_NoResize | ImGuiWindowFlags_HorizontalScrollbar |
-			ImGuiWindowFlags_NoSavedSettings;
-
-		ImGui::AlignTextToFramePadding();
-		ImGui::TextUnformatted("RECENT PROJECTS");
-		ImGui::Separator();
-		ImGui::BeginGroup();
-		{
-			if (ImGui::BeginChild("projects_content",
-				ImVec2(ImGui::GetContentRegionAvail().x * 0.7f, ImGui::GetContentRegionAvail().y),
-				false, flags))
-			{
-
-				
-			}
-			ImGui::EndChild();
-		}
-		ImGui::EndGroup();
-
-		ImGui::SameLine();
-
-		ImGui::BeginGroup();
-		{
-			if (ImGui::Button("NEW PROJECT", ImVec2(ImGui::GetContentRegionAvailWidth(), 0.0f)))
-			{
-				
-			}
-
-			if (ImGui::Button("OPEN OTHER", ImVec2(ImGui::GetContentRegionAvailWidth(), 0.0f)))
-			{
-				
-			}
-		}
-		ImGui::EndGroup();
-
-		ImGui::PopFont();
-
-		ImGui::End();
-	}
+	DrawStartPage(windowSize);
 	
 }
 
