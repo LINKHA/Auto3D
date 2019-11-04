8d3303d1d9d3234b7067af3db7c0adf600ad6819
LINKHA <421698843@qq.com>
2019-11-04 21:57:17 +0800

    #Remove excess resources

--- Auto3D/Source/Renderer/IBLMaterial.cpp
+++ Auto3D/Source/Renderer/IBLMaterial.cpp
@@ -169,104 +169,6 @@ namespace Auto3D
 		}
 	};
 
-
-// renders (and builds at first invocation) a sphere
-// -------------------------------------------------
-unsigned int sphereVAO = 0;
-unsigned int indexCount;
-void renderSphere()
-{
-	if (sphereVAO == 0)
-	{
-		glGenVertexArrays(1, &sphereVAO);
-
-		unsigned int vbo, ebo;
-		glGenBuffers(1, &vbo);
-		glGenBuffers(1, &ebo);
-
-		Vector<Vector3F> positions;
-		Vector<Vector2F> uv;
-		Vector<Vector3F> normals;
-		Vector<unsigned int> indices;
-
-		const unsigned int X_SEGMENTS = 64;
-		const unsigned int Y_SEGMENTS = 64;
-		const float PI = 3.14159265359;
-		for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
-		{
-			for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
-			{
-				float xSegment = (float)x / (float)X_SEGMENTS;
-				float ySegment = (float)y / (float)Y_SEGMENTS;
-				float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
-				float yPos = std::cos(ySegment * PI);
-				float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
-
-				positions.Push(Vector3F(xPos, yPos, zPos));
-				uv.Push(Vector2F(xSegment, ySegment));
-				normals.Push(Vector3F(xPos, yPos, zPos));
-			}
-		}
-
-		bool oddRow = false;
-		for (int y = 0; y < Y_SEGMENTS; ++y)
-		{
-			if (!oddRow) // even rows: y == 0, y == 2; and so on
-			{
-				for (int x = 0; x <= X_SEGMENTS; ++x)
-				{
-					indices.Push(y * (X_SEGMENTS + 1) + x);
-					indices.Push((y + 1) * (X_SEGMENTS + 1) + x);
-				}
-			}
-			else
-			{
-				for (int x = X_SEGMENTS; x >= 0; --x)
-				{
-					indices.Push((y + 1) * (X_SEGMENTS + 1) + x);
-					indices.Push(y * (X_SEGMENTS + 1) + x);
-				}
-			}
-			oddRow = !oddRow;
-		}
-		indexCount = indices.Size();
-
-		Vector<float> data;
-		for (int i = 0; i < positions.Size(); ++i)
-		{
-			data.Push(positions[i]._x);
-			data.Push(positions[i]._y);
-			data.Push(positions[i]._z);
-			if (uv.Size() > 0)
-			{
-				data.Push(uv[i]._x);
-				data.Push(uv[i]._y);
-			}
-			if (normals.Size() > 0)
-			{
-				data.Push(normals[i]._x);
-				data.Push(normals[i]._y);
-				data.Push(normals[i]._z);
-			}
-		}
-		glBindVertexArray(sphereVAO);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, data.Size() * sizeof(float), &data[0], GL_STATIC_DRAW);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.Size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
-		float stride = (3 + 2 + 3) * sizeof(float);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
-		glEnableVertexAttribArray(2);
-		glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, (void*)(5 * sizeof(float)));
-	}
-
-	glBindVertexArray(sphereVAO);
-	glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
-}
-
 // renderCube() renders a 1x1 3D cube in NDC.
 // -------------------------------------------------
 unsigned int cubeVAO = 0;
@@ -320,13 +222,9 @@ void renderCube()
 			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
 			-1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left        
 		};
-		//glGenVertexArrays(1, &cubeVAO);
 		glGenBuffers(1, &cubeVBO);
-		// fill buffer
 		glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-		// link vertex attributes
-		//glBindVertexArray(cubeVAO);
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(1);
@@ -334,12 +232,8 @@ void renderCube()
 		glEnableVertexAttribArray(2);
 		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		//glBindVertexArray(0);
 	}
-	// render Cube
-	//glBindVertexArray(cubeVAO);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
-	//glBindVertexArray(0);
 }
 
 // renderQuad() renders a 1x1 XY quad in NDC
@@ -358,9 +252,7 @@ void renderQuad()
 				1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
 		};
 		// setup plane VAO
-		//glGenVertexArrays(1, &quadVAO);
 		glGenBuffers(1, &quadVBO);
-		//glBindVertexArray(quadVAO);
 		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
 		glEnableVertexAttribArray(0);
@@ -368,65 +260,14 @@ void renderQuad()
 		glEnableVertexAttribArray(1);
 		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	}
-	//glBindVertexArray(quadVAO);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-	//glBindVertexArray(0);
 }
 
-unsigned int loadTexture(char const* path)
-{
-	unsigned int textureID;
-	glGenTextures(1, &textureID);
-
-	int width, height, nrComponents;
-	unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
-	if (data)
-	{
-		GLenum format;
-		if (nrComponents == 1)
-			format = GL_RED;
-		else if (nrComponents == 3)
-			format = GL_RGB;
-		else if (nrComponents == 4)
-			format = GL_RGBA;
-
-		glBindTexture(GL_TEXTURE_2D, textureID);
-		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		stbi_image_free(data);
-	}
-	else
-	{
-		std::cout << "Texture failed to load at path: " << path << std::endl;
-		stbi_image_free(data);
-	}
-
-	return textureID;
-}
-tShader pbrShader;
-tShader equirectangularToCubemapShader;
 tShader irradianceShader;
 tShader prefilterShader;
 tShader brdfShader;
-tShader backgroundShader;
-
-unsigned int goldAlbedoMap;
-unsigned int goldNormalMap;
-unsigned int goldMetallicMap;
-unsigned int goldRoughnessMap;
-unsigned int goldAOMap;
 
 
-unsigned int captureFBO;
-unsigned int captureRBO;
-
-unsigned int envCubemap;
 unsigned int irradianceMap;
 unsigned int prefilterMap;
 unsigned int brdfLUTTexture;
@@ -449,61 +290,6 @@ void IBLMaterial::RegisterObject()
 void IBLMaterial::CreatePass(Camera* camera)
 {
 
-	camera->SetPosition(Vector3F(0, 0, 3));
-		
-
-
-	while (1)
-	{
-		// render
-		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		Matrix4x4F model;
-		Matrix4x4F view = camera->GetViewMatrix();
-		view = Matrix4x4F(
-		1,0,0,0,
-		0,1,0,0,
-		0,0,1,0,
-		0,0,-3,1
-		);
-
-		// render scene, supplying the convoluted irradiance map to the final shader.
-	  // ------------------------------------------------------------------------------------------
-		pbrShader.use();
-
-		//Matrix4x4F view = camera->GetViewMatrix();
-		pbrShader.setMat4("view", view);
-		pbrShader.setVec3("camPos", camera->GetPosition());
-
-		// bind pre-computed IBL data
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, irradianceMap);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, prefilterMap);
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, brdfLUTTexture);
-
-
-		// gold
-		glActiveTexture(GL_TEXTURE3);
-		glBindTexture(GL_TEXTURE_2D, goldAlbedoMap);
-		glActiveTexture(GL_TEXTURE4);
-		glBindTexture(GL_TEXTURE_2D, goldNormalMap);
-		glActiveTexture(GL_TEXTURE5);
-		glBindTexture(GL_TEXTURE_2D, goldMetallicMap);
-		glActiveTexture(GL_TEXTURE6);
-		glBindTexture(GL_TEXTURE_2D, goldRoughnessMap);
-		glActiveTexture(GL_TEXTURE7);
-		glBindTexture(GL_TEXTURE_2D, goldAOMap);
-
-
-		model = Matrix4x4F();
-		pbrShader.setMat4("model", model);
-		renderSphere();
-
-		ModuleManager::Get().GraphicsModule()->Present();
-	}
 }
 
 void IBLMaterial::SetIrradiance()
@@ -523,46 +309,15 @@ void IBLMaterial::SetBrdfLut()
 
 void IBLMaterial::SetAAA(Texture* iblCube)
 {
-	// configure global opengl state
-// -----------------------------
-	glEnable(GL_DEPTH_TEST);
-	// set depth function to less than AND equal for skybox depth trick.
-	glDepthFunc(GL_LEQUAL);
-	// enable seamless cubemap sampling for lower mip levels in the pre-filter map.
-	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
-
-
-	// build and compile shaders
-// -------------------------
-	pbrShader = tShader("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.pbr.vs", "E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.pbr.fs");
-	equirectangularToCubemapShader = tShader("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.cubemap.vs", "E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.equirectangular_to_cubemap.fs");
 	irradianceShader = tShader("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.cubemap.vs", "E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.irradiance_convolution.fs");
 	prefilterShader = tShader("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.cubemap.vs", "E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.prefilter.fs");
 	brdfShader = tShader("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.brdf.vs", "E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.brdf.fs");
-	backgroundShader = tShader("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.background.vs", "E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/2.2.2.background.fs");
-
-	pbrShader.use();
-	pbrShader.setInt("irradianceMap", 0);
-	pbrShader.setInt("prefilterMap", 1);
-	pbrShader.setInt("brdfLUT", 2);
-	pbrShader.setInt("albedoMap", 3);
-	pbrShader.setInt("normalMap", 4);
-	pbrShader.setInt("metallicMap", 5);
-	pbrShader.setInt("roughnessMap", 6);
-	pbrShader.setInt("aoMap", 7);
-
-	backgroundShader.use();
-	backgroundShader.setInt("environmentMap", 0);
-
-	goldAlbedoMap = loadTexture("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/gold/albedo.png");
-	goldNormalMap = loadTexture("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/gold/normal.png");
-	goldMetallicMap = loadTexture("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/gold/metallic.png");
-	goldRoughnessMap = loadTexture("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/gold/roughness.png");
-	goldAOMap = loadTexture("E:/Project/MyProject/Auto3D/Auto3D/Source/Renderer/gold/ao.png");
 
 	// pbr: setup framebuffer
    // ----------------------
 
+	unsigned int captureFBO;
+	unsigned int captureRBO;
 	glGenFramebuffers(1, &captureFBO);
 	glGenRenderbuffers(1, &captureRBO);
 
@@ -708,16 +463,6 @@ void IBLMaterial::SetAAA(Texture* iblCube)
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-
-	// initialize static shader uniforms before rendering
-	// --------------------------------------------------
-	Matrix4x4F projection = Perspective(45.0f, (float)1024 / (float)768, 0.1f, 100.0f);
-	pbrShader.use();
-	pbrShader.setMat4("projection", projection);
-
-	// then before rendering, configure the viewport to the original framebuffer's screen dimensions
-	glViewport(0, 0, 1024, 768);
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 	Texture* tex1 = new Texture();
 	Texture* tex2 = new Texture();
