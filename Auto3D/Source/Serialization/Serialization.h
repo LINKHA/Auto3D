414d5b9e7841bb4d2e1e921cbe63467a9c3a03da
LINKHA <421698843@qq.com>
2019-12-15 21:19:32 +0800

    #Add deserialization-related properties

--- Auto3D/Source/Serialization/Serialization.h
+++ Auto3D/Source/Serialization/Serialization.h
@@ -1,6 +1,7 @@
 #include "AutoConfig.h"
 #include "IO/JSONValue.h"
 #include "Scene/Node.h"
+#include "Scene/Scene.h"
 #include "Resource/JSONFile.h"
 #include "Math/BoundingBox.h"
 #include "Math/AutoMath.h"
@@ -164,7 +165,6 @@ public:
 		SaveJSON(json.Root(), node);
 		return json.Save(dest);
 	}
-
 	void SaveJSON(FJSONValue& dest, ANode* node)
 	{
 		dest["type"] = RtToStr(FType::get(*node).get_name());
@@ -196,13 +196,12 @@ public:
 		for (auto& prop : type.get_properties())
 		{
 			if(prop.get_metadata(SERIALIZABLE))
-				SetProperty(dest, prop, node);
+				SaveProperty(dest, prop, node);
 		}
 	}
-	void SetProperty(FJSONValue& dest, const FProperty& prop, ANode* node)
+	void SaveProperty(FJSONValue& dest, const FProperty& prop, ANode* node)
 	{
 		FType type = prop.get_type();
-		FString ss = RtToStr(type.get_name());
 		FPropertyType propertyType(type);
 
 		switch (propertyType._type)
@@ -297,6 +296,170 @@ public:
 			break;
 		}
 	}
+
+	bool LoadRootJSON(FStream& source, AScene* scene)
+	{
+		AJSONFile json;
+		bool success = json.Load(source);
+		LoadRootJSON(json.Root(), scene);
+		return success;
+	}
+
+	bool LoadRootJSON(const FJSONValue& source, AScene* scene)
+	{
+		FString ownType = source["type"].GetString();
+		unsigned ownId = (unsigned)source["id"].GetNumber();
+
+		if (ownType != RtToStr(FType::get(*scene).get_name()))
+		{
+			ErrorString("Mismatching type of scene root node in scene file");
+			return false;
+		}
+
+		scene->Clear();
+
+		LoadJSON(source, scene);
+		return true;
+	}
+
+	void LoadJSON(const FJSONValue& source, ANode* node)
+	{
+		// Load property to node
+		LoadPropertyJSON(source, node);
+
+
+		const JSONArray& children = source["children"].GetArray();
+		if (children.Size())
+		{
+			for (auto it = children.Begin(); it != children.End(); ++it)
+			{
+				const FJSONValue& childJSON = *it;
+				FStringHash childType(childJSON["type"].GetString());
+				unsigned childId = (unsigned)childJSON["id"].GetNumber();
+				ANode* child = node->CreateChild(childType);
+				if (child)
+				{
+					LoadJSON(childJSON, child);
+				}
+			}
+		}
+
+	}
+
+	void LoadPropertyJSON(const FJSONValue& dest, ANode* node)
+	{
+		FType type = FType::get(*node);
+
+		for (auto& prop : type.get_properties())
+		{
+			if (prop.get_metadata(SERIALIZABLE))
+				LoadProperty(dest, prop, node);
+		}
+	}
+	void LoadProperty(const FJSONValue& source, const FProperty& prop, ANode* node)
+	{
+		FType type = prop.get_type();
+		FPropertyType propertyType(type);
+		FString typeName = RtToStr(type.get_name());
+
+		void* dest;
+		switch (propertyType._type)
+		{
+		case EPropertyType::BOOL:
+			*(reinterpret_cast<bool*>(dest)) = source.GetBool();
+			break;
+
+		case EPropertyType::BYTE:
+			*(reinterpret_cast<unsigned char*>(dest)) = (unsigned char)source.GetNumber();
+
+			break;
+		case EPropertyType::UNSIGNED:
+			*(reinterpret_cast<unsigned*>(dest)) = (unsigned)source.GetNumber();
+			break;
+
+		case EPropertyType::INT:
+			*(reinterpret_cast<int*>(dest)) = (int)source.GetNumber();
+			break;
+
+		case EPropertyType::INTVECTOR2:
+			TVector2F vec2 = source.GetVector2();
+			*reinterpret_cast<TVector2I*>(dest) = TVector2I(vec2._x, vec2._y);
+			break;
+
+		case EPropertyType::INTRECT:
+			TRectI rect = source.GetString();
+			*reinterpret_cast<TRectI*>(dest) = TRectI(rect._min._x, rect._min._y, rect._max._x, rect._max._y);
+			break;
+
+		case EPropertyType::FLOAT:
+			*(reinterpret_cast<float*>(dest)) = (float)source.GetNumber();
+			break;
+
+		case EPropertyType::VECTOR2:
+			*reinterpret_cast<TVector2F*>(dest) = source.GetVector2();
+			break;
+
+		case EPropertyType::VECTOR3:
+			*reinterpret_cast<TVector3F*>(dest) = source.GetVector3();
+			break;
+
+		case EPropertyType::VECTOR4:
+			*reinterpret_cast<TVector4F*>(dest) = source.GetVector4();
+			break;
+
+		case EPropertyType::QUATERNION:
+			reinterpret_cast<TVector4F*>(dest)->FromString(source.GetString());
+			break;
+
+		case EPropertyType::COLOR:
+			reinterpret_cast<FColor*>(dest)->FromString(source.GetString());
+			break;
+
+		case EPropertyType::RECT:
+			reinterpret_cast<TRectF*>(dest)->FromString(source.GetString());
+			break;
+
+		case EPropertyType::MATRIX2:
+			reinterpret_cast<TMatrix2x2F*>(dest)->FromString(source.GetString());
+			break;
+
+		case EPropertyType::MATRIX3:
+			reinterpret_cast<TMatrix3x3F*>(dest)->FromString(source.GetString());
+			break;
+
+		case EPropertyType::MATRIX3X4:
+			reinterpret_cast<TMatrix3x4F*>(dest)->FromString(source.GetString());
+			break;
+
+		case EPropertyType::MATRIX4:
+			reinterpret_cast<TMatrix4x4F*>(dest)->FromString(source.GetString());
+			break;
+
+		case EPropertyType::STRING:
+			*(reinterpret_cast<FString*>(dest)) = source.GetString();
+			break;
+
+		case EPropertyType::RESOURCEREF:
+			reinterpret_cast<FResourceRef*>(dest)->FromString(source.GetString());
+			break;
+
+		case EPropertyType::RESOURCEREFLIST:
+			reinterpret_cast<FResourceRefList*>(dest)->FromString(source.GetString());
+			break;
+
+		case EAttributeType::OBJECTREF:
+			reinterpret_cast<FObjectRef*>(dest)->_id = (unsigned)source.GetNumber();
+			break;
+
+		case EAttributeType::JSONVALUE:
+			*(reinterpret_cast<FJSONValue*>(dest)) = source;
+		default:
+			break;
+		}
+
+		prop.set_value(node, dest);
+	}
+
 };
 
 }
