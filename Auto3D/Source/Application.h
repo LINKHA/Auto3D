8528cc65515ebfa8039d5701bf5f856eb3219a34
LINKHA <421698843@qq.com>
2020-01-12 15:06:24 +0800

    #Eliminate redundant namespaces

--- Auto3D/Source/Application.h
+++ Auto3D/Source/Application.h
@@ -1,11 +1,29 @@
 #pragma once
 #include "AutoConfig.h"
+#include "Container/String.h"
+
+#include <memory>
 
 namespace Auto3D
 {
 
+class AEngine;
 /// The superclass implementation of the project space, where the engine is implemented
 class AUTO_API FApplication
 {
+public:
+	FApplication();
+	~FApplication();
+
+	/// This is AEngine important funcation init awake runloop and finish run
+	void Run();
+	/// Show an error message (last log message if empty), terminate the main loop, and set failure exit code.
+	void ErrorExit(const FString& message = FString::EMPTY);
+
+private:
+	/// Auto3D AEngine
+	//std::unique_ptr<AEngine> _engine;
+
 };
+
 }
