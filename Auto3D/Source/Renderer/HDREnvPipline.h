3e50f3af81975832d95e7c5f02fd1b1541c6f8b6
LINKHA <421698843@qq.com>
2020-05-21 14:16:01 +0800

    #Complete the preliminary setup of the creation of mipmap framebuffer

--- Auto3D/Source/Renderer/HDREnvPipline.h
+++ Auto3D/Source/Renderer/HDREnvPipline.h
@@ -24,28 +24,97 @@ public:
 	{
 		_equirectangularToCubemap.AttachShader("vs_cube_map", "fs_equirectangular_to_cubemap");
 		_irradianceConvolution.AttachShader("vs_cube_map", "fs_irradiance_convolution");
-		/*_prefilter.AttachShader("vs_cube_map", "fs_prefilter");*/
+		_prefilter.AttachShader("vs_cube_map", "fs_prefilter");
 
 		
 
 		_uniforms.Init();
 	}
 
-	void Update(ACameraComponent* camera,ASkyboxComponent* skybox)
+	void Update(ACameraComponent* camera, ASkyboxComponent* skybox)
 	{
 		GProcessWindow& processWindow = GProcessWindow::Get();
 
 		TMatrix4x4F environmentViewMatrix = camera->GetEnvironmentViewMatrix();
 		_uniforms._environmentViewMatrix = environmentViewMatrix.Transpose();
 		_uniforms._environmentViewTexture = skybox->GetHDRTexture()->GetTextureHandle();
+		static int sss = 0;
 
-		for (uint32_t ii = 0; ii < BX_COUNTOF(_uniforms._environmentViewTextureCubeFaceFb); ++ii)
+		if (!sss)
 		{
-			bgfx::ViewId viewId = bgfx::ViewId(ii + 1);
-			bgfx::setViewFrameBuffer(viewId, _uniforms._environmentViewTextureCubeFaceFb[ii]);
+			sss = 1;
+			for (uint32_t ii = 0; ii < BX_COUNTOF(_uniforms._environmentViewTextureCubeFaceFb); ++ii)
+			{
+				bgfx::ViewId viewId = bgfx::ViewId(ii + 1);
+				bgfx::setViewFrameBuffer(viewId, _uniforms._environmentViewTextureCubeFaceFb[ii]);
+
+				bgfx::setViewClear(viewId, BGFX_CLEAR_COLOR);
+				bgfx::setViewRect(viewId, 0, 0, 512, 512);
+
+				TMatrix4x4F captureProjection = Perspective(90.0f, 1.0f, 0.1f, 10.0f);
+				TMatrix4x4F captureViews[] =
+				{
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(1.0f,  0.0f,  0.0f), TVector3F(0.0f, -1.0f,  0.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(-1.0f,  0.0f,  0.0f), TVector3F(0.0f, -1.0f,  0.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  1.0f,  0.0f), TVector3F(0.0f,  0.0f,  1.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f, -1.0f,  0.0f), TVector3F(0.0f,  0.0f, -1.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  0.0f,  1.0f), TVector3F(0.0f, -1.0f,  0.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  0.0f, -1.0f), TVector3F(0.0f, -1.0f,  0.0f))
+				};
+
+				bgfx::setViewTransform(viewId, captureViews[ii].Data(), captureProjection.Data());
+				bgfx::setViewRect(viewId, 0, 0, uint16_t(512), uint16_t(512));
+
+				static OMesh* cubeMesh = GResourceModule::Get().LoadResource<OMesh>("Meshes/cube.bin");
+				static FGeometry* cubeGeometry = cubeMesh->CreateGeometry();
+
+				_uniforms.Submit();
+
+				Submit(cubeGeometry, viewId, NULL, _equirectangularToCubemap.GetProgram());
+			}
+
+
+			for (uint32_t ii = 0; ii < BX_COUNTOF(_uniforms._irradianceViewTextureCubeFaceFb); ++ii)
+			{
+				bgfx::ViewId viewId = bgfx::ViewId(ii + 7);
+				bgfx::setViewFrameBuffer(viewId, _uniforms._irradianceViewTextureCubeFaceFb[ii]);
+
+				bgfx::setViewClear(viewId, BGFX_CLEAR_COLOR);
+				bgfx::setViewRect(viewId, 0, 0, 32, 32);
+
+				TMatrix4x4F captureProjection = Perspective(90.0f, 1.0f, 0.1f, 10.0f);
+				TMatrix4x4F captureViews[] =
+				{
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(1.0f,  0.0f,  0.0f), TVector3F(0.0f, -1.0f,  0.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(-1.0f,  0.0f,  0.0f), TVector3F(0.0f, -1.0f,  0.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  1.0f,  0.0f), TVector3F(0.0f,  0.0f,  1.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f, -1.0f,  0.0f), TVector3F(0.0f,  0.0f, -1.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  0.0f,  1.0f), TVector3F(0.0f, -1.0f,  0.0f)),
+					LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  0.0f, -1.0f), TVector3F(0.0f, -1.0f,  0.0f))
+				};
+
+				bgfx::setViewTransform(viewId, captureViews[ii].Data(), captureProjection.Data());
+				bgfx::setViewRect(viewId, 0, 0, uint16_t(32), uint16_t(32));
+
+				static OMesh* cubeMesh = GResourceModule::Get().LoadResource<OMesh>("Meshes/cube.bin");
+				static FGeometry* cubeGeometry = cubeMesh->CreateGeometry();
+
+				//_uniforms.Submit();
+				bgfx::setTexture(0, _uniforms.us_equirectangulaCubeMap, _uniforms._environmentViewTextureCube);
+				Submit(cubeGeometry, viewId, NULL, _irradianceConvolution.GetProgram());
+			}
+		
+
+		for (uint32_t ii = 0; ii < BX_COUNTOF(_uniforms._prefilterTextureCubeFaceFb); ++ii)
+		{
+			unsigned int mip = ii / 6;
+			unsigned int mipSize = 128 /** std::pow(0.5, mip)*/;
+
+			bgfx::ViewId viewId = bgfx::ViewId(ii % 6 + 13);
+			bgfx::setViewFrameBuffer(viewId, _uniforms._prefilterTextureCubeFaceFb[ii]);
 
 			bgfx::setViewClear(viewId, BGFX_CLEAR_COLOR);
-			bgfx::setViewRect(viewId, 0, 0, 512, 512);
+			bgfx::setViewRect(viewId, 0, 0, mipSize, mipSize);
 
 			TMatrix4x4F captureProjection = Perspective(90.0f, 1.0f, 0.1f, 10.0f);
 			TMatrix4x4F captureViews[] =
@@ -59,38 +128,7 @@ public:
 			};
 
 			bgfx::setViewTransform(viewId, captureViews[ii].Data(), captureProjection.Data());
-			bgfx::setViewRect(viewId, 0, 0, uint16_t(512), uint16_t(512));
-
-			static OMesh* cubeMesh = GResourceModule::Get().LoadResource<OMesh>("Meshes/cube.bin");
-			static FGeometry* cubeGeometry = cubeMesh->CreateGeometry();
-
-			_uniforms.Submit();
-
-			Submit(cubeGeometry, viewId, NULL, _equirectangularToCubemap.GetProgram());
-		}
-
-
-		for (uint32_t ii = 0; ii < BX_COUNTOF(_uniforms._irradianceViewTextureCubeFaceFb); ++ii)
-		{
-			bgfx::ViewId viewId = bgfx::ViewId(ii + 7);
-			bgfx::setViewFrameBuffer(viewId, _uniforms._irradianceViewTextureCubeFaceFb[ii]);
-
-			bgfx::setViewClear(viewId, BGFX_CLEAR_COLOR);
-			bgfx::setViewRect(viewId, 0, 0, 512, 512);
-
-			TMatrix4x4F captureProjection = Perspective(90.0f, 1.0f, 0.1f, 10.0f);
-			TMatrix4x4F captureViews[] =
-			{
-				LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(1.0f,  0.0f,  0.0f), TVector3F(0.0f, -1.0f,  0.0f)),
-				LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(-1.0f,  0.0f,  0.0f), TVector3F(0.0f, -1.0f,  0.0f)),
-				LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  1.0f,  0.0f), TVector3F(0.0f,  0.0f,  1.0f)),
-				LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f, -1.0f,  0.0f), TVector3F(0.0f,  0.0f, -1.0f)),
-				LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  0.0f,  1.0f), TVector3F(0.0f, -1.0f,  0.0f)),
-				LookAt(TVector3F(0.0f, 0.0f, 0.0f), TVector3F(0.0f,  0.0f, -1.0f), TVector3F(0.0f, -1.0f,  0.0f))
-			};
-
-			bgfx::setViewTransform(viewId, captureViews[ii].Data(), captureProjection.Data());
-			bgfx::setViewRect(viewId, 0, 0, uint16_t(512), uint16_t(512));
+			bgfx::setViewRect(viewId, 0, 0, uint16_t(mipSize), uint16_t(mipSize));
 
 			static OMesh* cubeMesh = GResourceModule::Get().LoadResource<OMesh>("Meshes/cube.bin");
 			static FGeometry* cubeGeometry = cubeMesh->CreateGeometry();
@@ -99,8 +137,7 @@ public:
 			bgfx::setTexture(0, _uniforms.us_equirectangulaCubeMap, _uniforms._environmentViewTextureCube);
 			Submit(cubeGeometry, viewId, NULL, _irradianceConvolution.GetProgram());
 		}
-
-
+	}
 
 	}
 	struct Uniforms
@@ -108,9 +145,9 @@ public:
 		void Init()
 		{
 			us_equirectangularMap = bgfx::createUniform("s_equirectangularMap", bgfx::UniformType::Sampler);
-			//us_equirectangulaCubeMap = bgfx::createUniform("s_equirectangulaCubeMap", bgfx::UniformType::Sampler);
+			us_equirectangulaCubeMap = bgfx::createUniform("s_equirectangulaCubeMap", bgfx::UniformType::Sampler);
 
-			_environmentViewTextureCube = bgfx::createTextureCube(512, false, 1, bgfx::TextureFormat::BGRA8, BGFX_TEXTURE_RT);
+			_environmentViewTextureCube = bgfx::createTextureCube(512, false, 1, bgfx::TextureFormat::RGBA16F, BGFX_TEXTURE_RT);
 			for (uint32_t ii = 0; ii < BX_COUNTOF(_environmentViewTextureCubeFaceFb); ++ii)
 			{
 				bgfx::Attachment at;
@@ -118,12 +155,24 @@ public:
 				_environmentViewTextureCubeFaceFb[ii] = bgfx::createFrameBuffer(1, &at);
 			}
 
-			_irradianceViewTextureCube = bgfx::createTextureCube(512, false, 1, bgfx::TextureFormat::BGRA8, BGFX_TEXTURE_RT);
+			_irradianceViewTextureCube = bgfx::createTextureCube(32, false, 1, bgfx::TextureFormat::RGBA16F, BGFX_TEXTURE_RT);
 			for (uint32_t ii = 0; ii < BX_COUNTOF(_environmentViewTextureCubeFaceFb); ++ii)
 			{
 				bgfx::Attachment at;
 				at.init(_irradianceViewTextureCube, bgfx::Access::Write, uint16_t(ii));
 				_irradianceViewTextureCubeFaceFb[ii] = bgfx::createFrameBuffer(1, &at);
+			}
+
+			_prefilterTextureCube = bgfx::createTextureCube(128, true, 1, bgfx::TextureFormat::RGBA16F, BGFX_TEXTURE_RT);
+			unsigned int maxMipLevels = 5;
+			for (unsigned int mip = 0; mip < maxMipLevels; ++mip)
+			{
+				for (uint32_t ii = 0; ii < 6; ++ii)
+				{
+					bgfx::Attachment at;
+					at.init(_prefilterTextureCube, bgfx::Access::Write, uint16_t(ii), mip);
+					_prefilterTextureCubeFaceFb[mip * 6 + ii] = bgfx::createFrameBuffer(1, &at);
+				}
 			}
 		}
 
@@ -151,6 +200,9 @@ public:
 
 		bgfx::TextureHandle _irradianceViewTextureCube;
 		bgfx::FrameBufferHandle _irradianceViewTextureCubeFaceFb[6];
+
+		bgfx::TextureHandle _prefilterTextureCube;
+		bgfx::FrameBufferHandle _prefilterTextureCubeFaceFb[30];
 	};
 	
 	Uniforms _uniforms;
--- SampleProject/04-hdrEnv/fs_irradiance_convolution.sc
+++ SampleProject/04-hdrEnv/fs_irradiance_convolution.sc
@@ -7,32 +7,33 @@ SAMPLERCUBE(s_equirectangulaCubeMap, 0);
 void main()
 {		
 	float PI = 3.14159265359;
+	
+	vec3 N = normalize(v_position);
 
-    vec3 N = normalize(v_position);
+	vec3 irradiance = vec3_splat(0.0);
 
-    vec3 irradiance = vec3_splat(0.0);   
+	// tangent space calculation from origin point
+	vec3 up = vec3(0.0, 1.0, 0.0);
+	vec3 right = cross(up, N);
+	up = cross(N, right);
+
+	float sampleDelta = 0.025;
+	float nrSamples = 0.0f;
+	for (float phi = 0.0; phi < 2.0 * PI; phi += sampleDelta)
+	{
+		for (float theta = 0.0; theta < 0.5 * PI; theta += sampleDelta)
+		{
+			// spherical to cartesian (in tangent space)
+			vec3 tangentSample = vec3(mul(sin(theta), cos(phi)), mul(sin(theta), sin(phi)), cos(theta));
+			// tangent space to world
+			vec3 sampleVec = mul(tangentSample.x, right) + mul(tangentSample.y, up) + mul(tangentSample.z, N);
+
+			irradiance += mul(mul(textureCube(s_equirectangulaCubeMap, sampleVec).rgb, cos(theta)), sin(theta));
+			nrSamples++;
+		}
+	}
+	irradiance = mul(mul(PI, irradiance), (1.0 / float(nrSamples)));
+
     
-    // tangent space calculation from origin point
-    vec3 up    = vec3(0.0, 1.0, 0.0);
-    vec3 right = cross(up, N);
-    up         = cross(N, right);
-       
-    float sampleDelta = 0.025;
-    float nrSamples = 0.0f;
-    for(float phi = 0.0; phi < 2.0 * PI; phi += sampleDelta)
-    {
-        for(float theta = 0.0; theta < 0.5 * PI; theta += sampleDelta)
-        {
-            // spherical to cartesian (in tangent space)
-            vec3 tangentSample = vec3(mul(sin(theta), cos(phi)),  mul(sin(theta), sin(phi)), cos(theta));
-            // tangent space to world
-            vec3 sampleVec = mul(tangentSample.x, right) + mul(tangentSample.y, up) + mul(tangentSample.z, N); 
-
-            irradiance += mul(mul(textureCube(s_equirectangulaCubeMap, sampleVec).rgb, cos(theta)), sin(theta));
-            nrSamples++;
-        }
-    }
-    irradiance = mul(mul(PI, irradiance), (1.0 / float(nrSamples)));
-    
-    gl_FragColor = vec4(irradiance, 1.0);
+    gl_FragColor = vec4(0.5,0.5,0.5, 1.0);
 }
